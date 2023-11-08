a8cc2c1b75c8a2bb950e55ec19a8158aa583bde0
alanm3rcad0 <alanmercado@haipriority.com>
2023-11-08 16:51:23 -0400

    sprite changes directions accordingly

--- src/spritemovement.asm
+++ src/spritemovement.asm
@@ -6,17 +6,9 @@ player_x: .res 1
 player_y: .res 1
 player_dir_x: .res 1
 player_dir_y: .res 1
-sprite1_x: .res 1   ; Allocate memory for sprite 1 position
-sprite1_y: .res 1
-sprite1_dir_x: .res 1  ; Define direction for sprite 1
-sprite1_dir_y: .res 1
-sprite2_x: .res 1   ; Allocate memory for sprite 2 position
-sprite2_y: .res 1
-sprite2_dir_x: .res 1  ; Define direction for sprite 2
-sprite2_dir_y: .res 1
 ppuctrl_settings: .res 1
 pad1: .res 1
-.exportzp player_x, player_y, player_dir_x, player_dir_y, sprite1_x, sprite1_y, sprite1_dir_x, sprite1_dir_y, sprite2_x, sprite2_y, sprite2_dir_x, sprite2_dir_y, pad1
+.exportzp player_x, player_y, player_dir_x, player_dir_y, pad1
 
 .segment "CODE"
 .proc irq_handler
@@ -32,9 +24,9 @@ pad1: .res 1
   STA OAMDMA
 	LDA #$00
 
-  JSR read_controller1
 
   ; update tiles *after* DMA transfer
+  JSR read_controller1
 	JSR update_player
   JSR draw_player
 
@@ -44,6 +36,7 @@ pad1: .res 1
 .endproc
 
 .import reset_handler
+.import draw_platform
 
 .export main
 .proc main
@@ -93,87 +86,6 @@ forever:
   JMP forever
 .endproc
 
-.proc draw_platform
-  PHP
-  PHA
-  TXA
-  PHA
-  TYA
-  PHA
-
-  LDY #$e7
-
-first_row:
-  LDA PPUSTATUS
-	LDA #$21
-	STA PPUADDR
-	STY PPUADDR
-	LDX #$02
-	STX PPUDATA
-  INY
-  CPY #$f9
-  BNE first_row
-
-  LDY #$08
-
-second_row:
-  LDA PPUSTATUS
-	LDA #$22
-	STA PPUADDR
-	STY PPUADDR
-	LDX #$02
-	STX PPUDATA
-  INY
-  CPY #$18
-  BNE second_row
-
-  LDY #$29 
-
-third_row:
-  LDA PPUSTATUS
-	LDA #$22
-	STA PPUADDR
-	STY PPUADDR
-	LDX #$02
-	STX PPUDATA
-  INY
-  CPY #$37
-  BNE third_row
-
-  LDY #$4b
-
-fourth_row:
-  LDA PPUSTATUS
-	LDA #$22
-	STA PPUADDR
-	STY PPUADDR
-	LDX #$02
-	STX PPUDATA
-  INY
-  CPY #$55
-  BNE fourth_row
-
-  LDY #$6D
-
-fifth_row:
-  LDA PPUSTATUS
-	LDA #$22
-	STA PPUADDR
-	STY PPUADDR
-	LDX #$02
-	STX PPUDATA
-  INY
-  CPY #$73
-  BNE fifth_row
-
-  PLA
-  TAY
-  PLA
-  TAX
-  PLA
-  PLP
-  RTS
-.endproc
 
 .proc update_player
   PHP
@@ -183,78 +95,37 @@ fifth_row:
   TYA
   PHA
 
-  
   LDA pad1        ; Load button presses
   AND #BTN_LEFT   ; Filter out all but Left
   BEQ check_right ; If result is zero, left not pressed
-  DEC player_x  ; If the branch is not taken, move player left
+  DEC player_x    ; If the branch is not taken, move player left
+
+  LDA #$01
+  STA FACING
+
 check_right:
   LDA pad1
   AND #BTN_RIGHT
   BEQ check_up
   INC player_x
+
+  LDA #$00
+  STA FACING
+
 check_up:
   LDA pad1
   AND #BTN_UP
   BEQ check_down
   DEC player_y
+
 check_down:
   LDA pad1
   AND #BTN_DOWN
   BEQ done_checking
   INC player_y
+
 done_checking:
 
-;   ; Update player_x
-;   LDA player_x
-;   CMP #$e0
-;   BCC not_at_right_edge_x
-;   LDA #$00
-;   STA player_dir_x    ; Start moving left
-;   JMP direction_set_x
-; not_at_right_edge_x:
-;   LDA player_x
-;   CMP #$10
-;   BCS direction_set_x
-;   LDA #$01
-;   STA player_dir_x   ; Start moving right
-; direction_set_x:
-;   LDA player_dir_x
-;   CMP #$01
-;   BEQ move_right_x
-;   DEC player_x
-;   DEC player_x  ; Increase the decrement to move faster in X
-;   JMP exit_subroutine_xy
-; move_right_x:
-;   INC player_x
-;   INC player_x  ; Increase the increment to move faster in X
-
-  ; Update player_y
-;   LDA player_y
-;   CMP #$e0
-;   BCC not_at_bottom_edge_y
-;   LDA #$00
-;   STA player_dir_y    ; Start moving up
-;   JMP direction_set_y
-; not_at_bottom_edge_y:
-;   LDA player_y
-;   CMP #$10
-;   BCS direction_set_y
-;   LDA #$01
-;   STA player_dir_y   ; Start moving down
-; direction_set_y:
-;   LDA player_dir_y
-;   CMP #$01
-;   BEQ move_down_y
-;   DEC player_y
-;   DEC player_y  ; Increase the decrement to move faster in Y
-;   JMP exit_subroutine_xy
-; move_down_y:
-;   INC player_y
-;   INC player_y  ; Increase the increment to move faster in Y
-
-; exit_subroutine_xy:
-  ; All done, clean up and return
   PLA
   TAY
   PLA
@@ -273,7 +144,31 @@ done_checking:
   TYA
   PHA
 
-  ; write player ship tile numbers
+  ; LDA pad1
+  ; AND #BTN_LEFT
+  ; BEQ go_right
+
+  LDA FACING
+  CMP #%00000001
+  BEQ go_left
+  JMP go_right
+
+
+go_left:
+  LDA #$01
+  STA $0201
+  LDA #$00
+  STA $0205
+  LDA #$11
+  STA $0209
+  LDA #$10
+  STA $020d
+  LDA #$40
+  JMP continue
+
+go_right:
+  LDA pad1
+  AND #BTN_RIGHT
   LDA #$00
   STA $0201
   LDA #$01
@@ -282,10 +177,13 @@ done_checking:
   STA $0209
   LDA #$11
   STA $020d
+  LDA #$00
 
+  ; write player ship tile numbers
+
+continue:
   ; write player ship tile attributes
   ; use palette 0
-  LDA #$00
   STA $0202
   STA $0206
   STA $020a
